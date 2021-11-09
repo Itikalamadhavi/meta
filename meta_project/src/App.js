68bdebde354747637b665193661fd516e9a74b21
Madhavi Itikala <mitikala@innominds.com>
2021-11-09 17:00:10 +0530

    ok

---
+++ meta_project/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import './App.css';
+import Signup from './components/signup';
+import Login from './components/login';
+ import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+//import { Routes, Route, useParams } from "react-router-dom";
+
+function App() {
+  return (
+   
+     <Router>
+            <Switch>
+                <Route
+                    exact path="/"
+                    render={Login}
+                />
+                <Route
+                    exact path="/signup"
+                    render={Signup}
+                />
+        
+            </Switch>
+        </Router>
+  
+  );
+}
+
+export default App;
